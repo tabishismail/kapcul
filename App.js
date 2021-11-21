118aa2092d57d70c91b3c30081fc300f7a448933
tabishismail <tabish554@gmail.com>
2021-11-21 17:00:15 +0500

    navigation and ui building

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './src/config/Navigation';
+import { EditProfile } from './src/screens';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Navigation/>
+      {/* <EditProfile/> */}
+      {/* <StatusBar /> */}
     </View>
   );
 }
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F3FFFF',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
